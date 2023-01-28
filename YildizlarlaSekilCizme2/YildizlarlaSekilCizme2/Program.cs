96c70123c0a0a5945e8eeaa68de36b345cf726df
cengizhulya <hulyacengiz0@hotmail.com>
2023-01-28 23:36:23 +0300

    Yildizlarla Sekil Cizme 2
    Ters Dik Ucgen

---
+++ YildizlarlaSekilCizme2/YildizlarlaSekilCizme2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YildizlarlaSekilCizme2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ters Dik Ucgen Cizme
+            
+
+            for(int i=0; i<10; i++)
+            {
+                for (int j=10; j>i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadLine();
+            
+
+
+            /*
+            for (int i=10; i>0; i--)
+            {
+                for (int j=i; j>=1 ; j--)
+                {
+                    Console.Write("*");
+                   
+                }
+
+                Console.WriteLine("");
+
+            }
+
+            Console.ReadLine();
+            */
+        }
+    }
+}
