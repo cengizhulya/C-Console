c86506917b5e78d3f1dd9efff34e286f22486d6b
cengizhulya <hulyacengiz0@hotmail.com>
2023-01-28 23:08:41 +0300

    Yildizlarla Sekil Cizme 1
    Dik Ucgen

---
+++ YildizlarlaSekilCizme1/YildizlarlaSekilCizme1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YildizlarlaSekilCizme1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dik Ucgen
+
+            for(int i = 1; i <= 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
