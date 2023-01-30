628f1120f771f677827ea2fe98bb5c78ee799694
cengizhulya <hulyacengiz0@hotmail.com>
2023-01-30 23:05:28 +0300

    Yildizlarla Sekil Cizme 4

---
+++ YildizlarlaSekilCizme4/YildizlarlaSekilCizme4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YildizlarlaSekilCizme4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int deger;
+            Console.Write("Baklava diliminin buyukluk sayisini girin:");
+            deger = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i<=deger; i++)
+            {
+                for(int j = deger; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for(int k =1; k <= i; k++)
+                {
+                    Console.Write("*"+" ");
+                }
+                Console.WriteLine();
+            }
+
+            for(int l=1;l<=deger; l++)
+            {
+                for(int m=1; m <= l; m++)
+                {
+                    Console.Write(" ");
+                }
+
+                for(int n = deger; n > l; n--)
+                {
+                    Console.Write("*" + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
