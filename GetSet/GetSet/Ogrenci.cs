f6b42ff32b422c3014341863dd5ccb463fcf732e
cengizhulya <hulyacengiz0@hotmail.com>
2023-01-31 23:53:04 +0300

    Geter Seter

---
+++ GetSet/GetSet/Ogrenci.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSet
+{
+    class Ogrenci
+    {
+        public string ad;
+
+        private string soyad;
+
+        private int yas;
+
+        public string AD
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+
+        public string SOYAD
+        {
+            get { return soyad; }
+            set { soyad = value; }
+        }
+
+        public int YAS
+        {
+            get { return yas; }
+
+          set
+            {
+                if (value < 18)
+                {
+                    yas = 18;
+                }
+                else
+                {
+                    yas = value;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ GetSet/GetSet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSet
+{
+    class Metotlar
+    {
+    
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci_ = new Ogrenci();
+
+            ogrenci_.AD = "Hilal";
+            ogrenci_.SOYAD = "Cengiz";
+            ogrenci_.YAS = 21;
+
+            Console.WriteLine("Adi:"+ogrenci_.AD);
+            Console.WriteLine("Soyadi:"+ogrenci_.SOYAD);
+            Console.WriteLine("Yas:"+ogrenci_.YAS);
+
+            Console.ReadLine();
+        }
+    }
+}
