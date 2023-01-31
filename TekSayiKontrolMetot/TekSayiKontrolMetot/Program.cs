e2465799d8e9321aa8b46701c2a79c70fb6b9721
cengizhulya <hulyacengiz0@hotmail.com>
2023-01-31 23:52:47 +0300

    Tek Sayi Kontrol
    Tek Sayi Kontrol Metotla

---
+++ TekSayiKontrolMetot/TekSayiKontrolMetot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekSayiKontrolMetot
+{
+    class Program
+    {
+        //Klavyeden girilerek parametre olarak gönderilen bir sayının, tek olup olmadığını kontrol eden, eğer sayı tek ise true, değilse false değeri döndüren metodu yazınız.
+
+        private static bool tekKontrol (int sayi)
+        {
+            if (sayi % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            int sayi_;
+
+            Console.Write("Bir sayi girin:");
+
+            sayi_ = int.Parse(Console.ReadLine());
+
+            bool cikti = tekKontrol(sayi_);
+
+            Console.WriteLine(cikti);
+
+            Console.ReadLine();
+        }
+    }
+    
+}
